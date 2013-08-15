5c7926f614839ff09b03a76fa3aa03cfa098b421
ForbesLindesay <forbes@lindesay.co.uk>
2013-08-15 01:55:43 +0100

    The beginnings of some tests

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 'use strict'
 
-var color = require('bash-color')
 var util = require('util')
+var path = require('path')
+
+var color = require('bash-color')
+var mongojs = require('mongojs')
 
 var startup = (new Date()).toISOString()
-var id = -100000000000000
+var requestID = 0
+var lastMessage = (new Date()).toISOString()
+var messageID = 0
 
-module.exports = console
+module.exports = makeConsole
 module.exports.output = output
+module.exports.redirect = redirect
+module.exports.console = makeConsole
+module.exports.root = root
+module.exports.rootPath = process.cwd()
 
-function console(filename, startTime) {
+module.exports.requests = requests
+module.exports.errors = errors
+module.exports.handleGlobalErrors = handleGlobalErrors
+
+function makeConsole(filename, startTime) {
   var csl = {}
   csl.log = logger(filename, startTime, 'log')
   csl.info = logger(filename, startTime, 'info')
   csl.warn = logger(filename, startTime, 'warn')
   csl.error = logger(filename, startTime, 'error')
   csl.time = function () {
-    return console(filename, new Date())
+    return makeConsole(filename, new Date())
   }
   return csl
 }
@@ -26,13 +39,13 @@ function logger(filename, start, level) {
     var result = {type: type, timestamp: new Date()}
     if (type === 'server/request') {
       obj = obj.req
-      obj.__chronicle_id__ = startup + '-' + (id++)
+      obj.__chronicle_id__ = startup + '-' + (requestID++)
       obj.__chronicle_start__ = new Date()
       result.requestID = obj.__chronicle_id__
       result.httpVersion = obj.httpVersion
-      result.headers = obj.req.headers
-      result.method = obj.req.method
-      result.url = obj.req.url
+      result.headers = obj.headers
+      result.method = obj.method
+      result.url = obj.url
     } else if (type === 'server/response') {
       result.requestID = obj.req.__chronicle_id__
       result.statusCode = obj.res.statusCode
@@ -49,6 +62,10 @@ function logger(filename, start, level) {
         result[key] = obj[key]
       }
     }
+    if (obj.req && obj.req.__chronicle_id__) {
+      result.requestID = obj.req.__chronicle_id__
+      if ('req' in result) delete result.req
+    }
     if (start) {
       result.duration = ((new Date()) - start)
     }
@@ -64,7 +81,7 @@ function logger(filename, start, level) {
 
 
 var loggingRequests = {}
-function output(message, indent) {
+function output(message, indent, callback) {
   indent = indent || ''
   if (message.requestID && message.type === 'server/request') {
     loggingRequests[message.requestID] = {
@@ -95,7 +112,7 @@ function output(message, indent) {
   var type = color.cyan(message.type) + ' '
   switch (message.type) {
     case 'db/call':
-      log(type + message.name + message.args.map(formatInline).join(', ') + duration)
+      log(type + message.name + '(' + message.args.map(formatInline).join(', ') + ')' + duration)
       break
     case 'db/update':
       var index = 0
@@ -119,6 +136,9 @@ function output(message, indent) {
       indent += '  '
       log(message.raw ? message.message : util.inspect(message, false, 10, true))
   }
+  if (typeof callback === 'function') {
+    setTimeout(callback, 0)
+  }
 }
 function outputRequest(requestID) {
   if (!loggingRequests[requestID]) return
@@ -127,8 +147,16 @@ function outputRequest(requestID) {
   loggingRequests[requestID] = null
 
   var log = console[msg.res.level] || console.log
-  console.log(msg.req.method.toUpperCase() + ' ' + color[c](msg.res.statusCode || 'TIMEOUT') + msg.req.url
-    + color.purple(' (' + (message.res.duration || (message.res.timestamp - message.req.timestamp)) + 'ms)'))
+  var c = 'green'
+  if (msg.res.statusCode >= 300 && msg.res.statusCode < 400) {
+    c = 'purple'
+  } else if (msg.res.statusCode >= 400 && msg.res.statusCode < 500) {
+    c = 'yellow'
+  } else if (msg.res.statusCode >= 500 || !msg.res.statusCode) {
+    c = 'red'
+  }
+  console.log(msg.req.method.toUpperCase() + ' ' + color[c](msg.res.statusCode || 'TIMEOUT') + ' ' + msg.req.url
+    + color.purple(' (' + (msg.res.duration || (msg.res.timestamp - msg.req.timestamp)) + 'ms)'))
   for (var i = 0; i < msg.logs.length; i++) {
     output(msg.logs[i], '  ')
   }
@@ -138,6 +166,77 @@ function formatInline(arg) {
   return util.inspect(arg, false, 3, true).replace(/^ */gm, '').replace(/ *$/gm, ' ').replace(/\n/g, '').trim()
 }
 
+
+function redirect(db, collection) {
+  if (mongo !== undefined) {
+    db = mongojs(db, [collection || 'chronicle'])
+    module.exports.output = function (message, indent, callback) {
+      var timestamp = new Date()
+      if (timestamp !== lastMessage) {
+        lastMessage = timestamp
+        messageID++
+      }
+      message._id = timestamp + '-' + (messageID++)
+      if (message.filename) {
+        message.filename = path.relative(module.exports.rootPath, message.filename)
+      }
+      db[collection || 'chronicle'].insert(message, function (err) {
+        if (err) console.error(err.stack || err.message || err)
+        if (typeof callback === 'function') {
+          callback(err)
+        }
+      })
+    }
+  }
+  return module.exports
+}
+function root(path) {
+  module.exports.rootPath = path
+  return module.exports
+}
+function requests(filename) {
+  return function (req, res, next) {
+    logger(filename, null, 'log')('server/request', {req: req})
+    var end = res.end;
+    res.end = function(chunk, encoding){
+      res.end = end;
+      res.end(chunk, encoding);
+      logger(filename, null, res.statusCode >= 400 ? 'error' : 'log')('server/response', {
+        req: req,
+        res: res
+      })
+    };
+    next()
+  }
+}
+function errors(filename, options) {
+  return function (err, req, res, next) {
+    logger(filename, null, 'error')('server/error', {
+      message: err.stack || err.message || err,
+      raw: true,
+      req: req
+    })
+    if (!(options && options.last)) next(err)
+  }
+}
+
+function handleGlobalErrors() {
+  process.once('uncaughtException', onUncaughtException);
+  module.exports.handleGlobalErrors = function () {}
+  return module.exports
+}
+
+function onUncaughtException(err) {
+  output({
+    type: 'application/crash',
+    level: 'error',
+    message: err.stack || err.message || err,
+    raw: true,
+    timestamp: new Date()
+  }, '', function () {
+    throw err
+  })
+}
 
 
 function clone(obj, circular) {
---
+++ test/index.js
@@ -0,0 +1,101 @@
+var csl = require('../')(__filename)
+
+csl.log('anything/random', {
+  random: 'objects can be logged'
+})
+csl.log('cache/invalidate', {
+  partition: 'part',
+  row: 'row'
+})
+csl.log('email/batch', {
+  size: 3
+})
+csl.log('email/sent', {
+  subject: 'MessageA',
+  to: 'me myself and I'
+})
+csl.log('email/sent', {
+  subject: 'MessageB',
+  to: 'me myself and I'
+})
+csl.log('email/sent', {
+  subject: 'MessageC',
+  to: 'me myself and I'
+})
+
+
+function testTiming(csl, req, cb) {
+  setTimeout(function () {
+    csl.info('db/call', {
+      name: 'database-methods',
+      args: ['are', 'formatted', 'specially'],
+      req: req
+    })
+    csl.info('db/update', {
+      statement: 'UPDATE SET update-statements=?',
+      values: ['handled'],
+      req: req
+    })
+    cb()
+  }, 200)
+}
+
+var request = require('request')
+var chronicle = require('../')
+var app = require('express')()
+
+app.use(chronicle.requests(__filename))
+app.get('/', function (req, res) {
+  testTiming(csl.time(), req, function () {
+    res.json(true)
+  })
+})
+app.get('/whoa', function (req, res, next) {
+  testTiming(csl.time(), req, function () {
+    next(new Error('simulated database error'))
+  })
+})
+app.get('/timeout', function (req, res, next) {
+  testTiming(csl.time(), req, function () {
+  })
+})
+app.use(chronicle.errors(__filename))
+app.use(function (err, req, res) {
+  // default to 500
+  if (res.statusCode < 400) res.statusCode = 500
+
+  // respect err.status
+  if (err.status) res.statusCode = err.status
+
+  // production gets a basic error message
+  var msg = 'production' == env
+    ? http.STATUS_CODES[res.statusCode]
+    : err.stack || err.toString()
+
+  if (res.headerSent) return req.socket.destroy()
+  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Content-Length', Buffer.byteLength(msg))
+  if ('HEAD' == req.method) return res.end()
+  res.end(msg)
+})
+app.listen(3000)
+
+request('http://localhost:3000/', function (err) {
+  if (err) throw err
+})
+request('http://localhost:3000/', function (err) {
+  if (err) throw err
+})
+request('http://localhost:3000/', function (err) {
+  if (err) throw err
+})
+request('http://localhost:3000/whoa', function (err) {
+  if (err) throw err
+})
+request('http://localhost:3000/timeout', function (err) {
+  if (err) throw err
+})
+
+setTimeout(function () {
+  process.exit(0)
+}, 15000)
